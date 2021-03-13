85b212b3c9c7cbde3ffd932477a2618ef62dc56b
Ethan Johnson <ethan.johnson@telus.com>
2021-03-12 22:03:48 -0500

    started landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonPage, IonGrid, IonRow } from "@ionic/react";
+
+import "./Home.css";
+import React from "react";
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonGrid>
+          <IonRow>
+            <div className="sign">
+              <span className="fast-flicker">Open </span> Capsu
+              <span className="flicker">l</span>e
+            </div>
+          </IonRow>
+          <div className="container">
+            <a href="">Sign Up</a>
+            <br />
+            <a href="">Login</a>
+          </div>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
